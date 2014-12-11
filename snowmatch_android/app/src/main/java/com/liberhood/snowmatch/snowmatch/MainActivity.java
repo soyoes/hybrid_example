8503a376f6993b35ca6926b6dae700c7b7aebcee
soyoes <soyoes@gmail.com>
2014-12-11 19:50:54 +0900

    remove cross domain check for android

--- snowmatch_android/app/src/main/java/com/liberhood/snowmatch/snowmatch/MainActivity.java
+++ snowmatch_android/app/src/main/java/com/liberhood/snowmatch/snowmatch/MainActivity.java
@@ -6,17 +6,25 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
+import android.webkit.WebSettings;
 
 
 public class MainActivity extends Activity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView wv= (WebView)findViewById(R.id.webview);
+        
+        WebSettings settings = wv.getSettings();
+        
+        settings.setJavaScriptEnabled(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+        
         wv.loadUrl("file:///android_asset/index.html");
         wv.getSettings().setJavaScriptEnabled(true);
     }
-
+    
 }
